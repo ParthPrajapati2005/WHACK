e81cbbc8d948c4c133a890f8f3c45a5a296e420f
jackiewaang <zjackiewang03@gmail.com>
2024-10-26 19:08:38 +0100

    added error page

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,8 +1,20 @@
+import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 function ErrorPage(){
+
+    const navigate = useNavigate();
+
+    function goBack(){
+        navigate('/');
+    }
+
     return(
-        <>
-        </>
+        <div className='flex flex-col justify-center h-screen items-center'>
+            <h1>Ops, Nothing to see here.</h1>
+            <Button variant="primary" onClick={goBack}>Go to the previous page</Button>
+        </div>
     );
 }
 
