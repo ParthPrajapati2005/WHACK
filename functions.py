5847cd5087d915f33fc764cfa50e6c22a295b33e
Krishan <krishansachdev@hotmail.co.uk>
2024-10-26 17:32:12 +0100

    added calc function (short for calculator)

--- functions.py
+++ functions.py
@@ -7,5 +7,9 @@ interestRates = {
 }
 
 
-def calculateTotalDebtAtEndOfGraduation():
-    pass
+def calculateTotalDebtAtEndOfGraduation(startYear, endYear, maintenance):
+    tot = 0
+    for i in range(startYear+1,endYear+1):
+        tot += (9250 + maintenance) * pow((1+interestRates.get(i)/100),i-startYear)
+    return tot
+
