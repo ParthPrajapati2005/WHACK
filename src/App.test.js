3d3419140ae4c139558392d8c50316d17220f8e3
jackiewaang <zjackiewang03@gmail.com>
2024-10-26 15:17:14 +0100

    I removed app.css and test file

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'; // I am here not
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
