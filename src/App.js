3b35ea1c6d27b055a88e960c3f65fbfc2b5f6328
parth13075 <109304629+parth13075@users.noreply.github.com>
2024-10-26 13:53:26 +0100

    added blue bg and btn

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Button } from 'react-bootstrap';
+import './App.css'; // Import custom CSS for styling
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="d-flex justify-content-center align-items-center vh-100 bg-blue">
+      <Button variant="primary">Click Me!</Button>
+    </Container>
   );
 }
 
