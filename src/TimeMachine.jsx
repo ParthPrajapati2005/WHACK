0582e38b6fe3e954affe74cbdbdd2d7b832a969d
jackiewaang <zjackiewang03@gmail.com>
2024-10-27 10:42:07 +0000

    present this

--- src/TimeMachine.jsx
+++ src/TimeMachine.jsx
@@ -92,7 +92,6 @@ function TimeMachine() {
                                     required // Make field required
                                 />
 
-<<<<<<< HEAD
                                 <MDBInput
                                     wrapperClass='mb-4'
                                     placeholder='How many years is your degree?'
@@ -102,17 +101,6 @@ function TimeMachine() {
                                     onChange={handleInputChange}
                                     required // Make field required
                                 />
-=======
-                            <MDBInput
-                                wrapperClass='mb-4'
-                                placeholder='When is the end year of your degree?'
-                                id='end'
-                                type='number'
-                                value={formData.end}
-                                onChange={handleInputChange}
-                                required // Make field required
-                            />
->>>>>>> b2cd44a95ebabaa7529c583ab89f0a7fd21dc96e
 
                                 <MDBInput
                                     wrapperClass='mb-4'
@@ -124,7 +112,6 @@ function TimeMachine() {
                                     required // Make field required
                                 />
                                 
-<<<<<<< HEAD
                                 <MDBCardText>Enter your degree type below:</MDBCardText>
                                 <select
                                     id="degreeType"
@@ -242,91 +229,6 @@ function TimeMachine() {
                                 <p><strong>Years : </strong>{JSON.stringify(result.years, null, 2)}</p>
                             </div>
                         )}
-=======
-                                <option value="Aberdeenshire">Aberdeenshire</option>
-                                <option value="Angus kincardineshire">Angus & Kincardineshire</option>
-                                <option value="Antrim">Antrim</option>
-                                <option value="Argyll">Argyll</option>
-                                <option value="Armagh">Armagh</option>
-                                <option value="Avon">Avon</option>
-                                <option value="Ayrshire wigtownshire kirkcudbrightshire">Ayrshire, Wigtownshire & Kirkcudbrightshire</option>
-                                <option value="Bedfordshire">Bedfordshire</option>
-                                <option value="Berkshire">Berkshire</option>
-                                <option value="Berwickshire roxburghshire east-lothian">Berwickshire, Roxburghshire & East Lothian</option>
-                                <option value="Buckinghamshire">Buckinghamshire</option>
-                                <option value="Cambridgeshire">Cambridgeshire</option>
-                                <option value="Cheshire">Cheshire</option>
-                                <option value="Cleveland">Cleveland</option>
-                                <option value="Clwyd">Clwyd</option>
-                                <option value="Cornwall">Cornwall</option>
-                                <option value="County-durham">County Durham</option>
-                                <option value="Cumbria isle of man">Cumbria & Isle of Man</option>
-                                <option value="Derbyshire">Derbyshire</option>
-                                <option value="Devon">Devon</option>
-                                <option value="Dorset">Dorset</option>
-                                <option value="Down">Down</option>
-                                <option value="Dumfriesshire peeblesshire selkirkshire">Dumfriesshire, Peeblesshire & Selkirkshire</option>
-                                <option value="Dunbartonshire">Dunbartonshire</option>
-                                <option value="Durham">Durham</option>
-                                <option value="Dyfed">Dyfed</option>
-                                <option value="East sussex">East Sussex</option>
-                                <option value="East yorkshire-humberside">East Yorkshire & Humberside</option>
-                                <option value="Essex">Essex</option>
-                                <option value="Fermanagh tyrone">Fermanagh & Tyrone</option>
-                                <option value="Fife">Fife</option>
-                                <option value="Gloucestershire">Gloucestershire</option>
-                                <option value="Greater london">Greater London</option>
-                                <option value="Greater manchester">Greater Manchester</option>
-                                <option value="Gwent">Gwent</option>
-                                <option value="Gwynedd">Gwynedd</option>
-                                <option value="Hampshire isle of wight">Hampshire & Isle of Wight</option>
-                                <option value="Herefordshire worcestershire">Herefordshire & Worcestershire</option>
-                                <option value="Hertfordshire">Hertfordshire</option>
-                                <option value="Inverness shire banffshire">Inverness-Shire & Banffshire</option>
-                                <option value="Kent">Kent</option>
-                                <option value="Lanarkshire">Lanarkshire</option>
-                                <option value="Lancashire">Lancashire</option>
-                                <option value="Leicestershire">Leicestershire</option>
-                                <option value="Lincolnshire">Lincolnshire</option>
-                                <option value="Londonderry">Londonderry</option>
-                                <option value="Lothian">Lothian</option>
-                                <option value="Merseyside">Merseyside</option>
-                                <option value="Mid glamorgan">Mid Glamorgan</option>
-                                <option value="Middlesex">Middlesex</option>
-                                <option value="Midlothian">Midlothian</option>
-                                <option value="Morayshire">Morayshire</option>
-                                <option value="Norfolk">Norfolk</option>
-                                <option value="North-yorkshire">North Yorkshire</option>
-                                <option value="Northamptonshire">Northamptonshire</option>
-                                <option value="Northumberland">Northumberland</option>
-                                <option value="Nottinghamshire">Nottinghamshire</option>
-                                <option value="Oxfordshire">Oxfordshire</option>
-                                <option value="Perthshire clackmannanshire">Perthshire & Clackmannanshire</option>
-                                <option value="Powys">Powys</option>
-                                <option value="Renfrewshire">Renfrewshire</option>
-                                <option value="Ross shire sutherland caithness lewis orkney shetland">Ross-Shire, Sutherland, Caithness, Lewis, Orkney & Shetland</option>
-                                <option value="Rutland">Rutland</option>
-                                <option value="Shropshire">Shropshire</option>
-                                <option value="Somerset">Somerset</option>
-                                <option value="South glamorgan">South Glamorgan</option>
-                                <option value="South yorkshire">South Yorkshire</option>
-                                <option value="Staffordshire">Staffordshire</option>
-                                <option value="Stirlingshire">Stirlingshire</option>
-                                <option value="Suffolk">Suffolk</option>
-                                <option value="Surrey">Surrey</option>
-                                <option value="Tyne and wear">Tyne and Wear</option>
-                                <option value="Warwickshire">Warwickshire</option>
-                                <option value="West glamorgan">West Glamorgan</option>
-                                <option value="West lothian">West Lothian</option>
-                                <option value="West midlands">West Midlands</option>
-                                <option value="West sussex">West Sussex</option>
-                                <option value="West yorkshire">West Yorkshire</option>
-                                <option value="Wiltshire">Wiltshire</option>
-                            </select>
-  
-                            <Button className='w-100' size='md' type="submit">Submit</Button>
-                        </form>
->>>>>>> b2cd44a95ebabaa7529c583ab89f0a7fd21dc96e
                     </MDBCardBody>
                 </MDBCard>
             </div>
